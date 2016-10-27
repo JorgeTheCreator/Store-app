2e798aa1e744bb1928f39447f579e576e839116b
Jorge Peguero <JrgPeguero@yahoo.com>
2016-10-27 16:06:48 -0400

    pictures

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -7,7 +7,7 @@ var prompt = require("prompt");
 var connection = mysql.createConnection({
     host : "localhost",
     user : "root",
-    password : "",
+    password : "private",
     database : "Bamazon"
 });
 
